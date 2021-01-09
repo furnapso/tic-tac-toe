807d74477728fdad8ffaca25b678d94cdb56de26
furnapso <luke.boland95@gmail.com>
2021-01-09 21:57:33 +1000

    feat: added convert functions

--- script.js
+++ script.js
@@ -7,21 +7,33 @@ const gameBoard = (() => {
 
     const squares = document.querySelectorAll(".square");
 
-    const convertCoordinates = (coordinates) => {
+    const convertFromCoordinates = (coordinates) => {
         return board[0].length * coordinates[0] + coordinates[1];
+    }
+
+    const convertToCoordinates = (index) => {
+        return [
+            Math.floor(index / board[0].length),
+            index % board[0].length
+        ]
     }
 
     const drawBoard = () => {
         for (let y = 0; y < board.length; y++) {
             for (let x = 0; x < board[y].length; x++) {
-                squares[convertCoordinates([y, x])].classList.toggle(board[y][x]);
+                squares[convertFromCoordinates([y, x])].classList.toggle(board[y][x]);
             }
         }
     }
+
+    squares.forEach(square => {
+        square.addEventListener('click', (event) => {
+            // TODO: Add board update logic
+            console.log(Array.from(squares).indexOf(event.target));
+        })
+    })
 
     return {
         drawBoard
     }
 })();
-
-gameBoard.drawBoard();
